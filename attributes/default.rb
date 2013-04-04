9de41af8b68c596b9e2ead57676f696a7743c49a
Matt Ray <matthewhray@gmail.com>
2013-04-04 12:28:56 -0500

    attribute for adding emacs 24 support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,28 +1,34 @@
-# 
+#
 # Author:: Joshua Timberman <joshua@opscode.com>
-# Copyright:: Copyright (c) 2011, Opscode, Inc.
+# Copyright:: Copyright (c) 2011-2013, Opscode, Inc.
 # License:: Apache License, Version 2.0
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
+
+default['emacs']['24'] = false
 
 case node['platform']
 when "ubuntu","debian"
-  default['emacs']['packages'] = ["emacs23-nox"]
+  if node['emacs']['24']
+    default['emacs']['packages'] = ["emacs24-nox"]
+  else
+    default['emacs']['packages'] = ["emacs23-nox"]
+  end
 when "redhat","centos","scientific","fedora","arch"
   default['emacs']['packages'] = ["emacs-nox"]
 when "freebsd"
   default['emacs']['packages'] = ["editors/emacs-nox11"]
 else
-  default['emacs']['packages'] = ["emacs"]  
+  default['emacs']['packages'] = ["emacs"]
 end
